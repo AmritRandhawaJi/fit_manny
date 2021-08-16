840e13212a4fecb258c5183754ffb10606ef86e5
Amrit <Amrit.randhawa.ji@gmail.com>
2021-08-16 18:56:56 +0530

    16/aug/2021

---
+++ lib/model/drawerLayout.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+ class DrawerLayout{
+  static Widget layout(){
+    return Column(
+      children: [
+      Text("Drawer")
+      ],
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,19 +1,28 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fit_manny/model/drawerLayout.dart';
 import 'package:fit_manny/model/firebase.dart';
 import 'package:fit_manny/screens/mainScreens/profile.dart';
 import 'package:fit_manny/widgets/errors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:sliding_up_panel/sliding_up_panel.dart';
+import 'package:rive/rive.dart';
 
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+
+  @override
+  void initState() {
+   _validate();
+    super.initState();
+  }
+
+
   FirebaseFirestore server = FirebaseFirestore.instance;
 
   Future<dynamic> _validate() async {
@@ -35,84 +44,62 @@ class _HomeState extends State<Home> {
   }
 
   @override
-  void initState() {
-    _validate();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
           actions: [
             Padding(
               padding: const EdgeInsets.only(right: 20),
-              child: GestureDetector(onTap: (){
-                if (Platform.isIOS) {
-                  Navigator.of(context).push(CupertinoPageRoute(builder: (context) => Profile(),));
-                }
-                 if (Platform.isAndroid) {
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => Profile(),));
-                }              },child: Icon(Icons.account_circle_outlined,color: Colors.black,)),
+              child: GestureDetector(
+                  onTap: () {
+                    if (Platform.isIOS) {
+                      Navigator.of(context).push(CupertinoPageRoute(
+                        builder: (context) => Profile(),
+                      ));
+                    }
+                    if (Platform.isAndroid) {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => Profile(),
+                      ));
+                    }
+                  },
+                  child: Icon(
+                    Icons.account_circle_outlined,
+                    color: Colors.black,
+                  )),
             )
           ],
-            elevation: 0,
-            backgroundColor: Colors.white,
-            leading: Builder(builder: (BuildContext context) {
-              return IconButton(
-                icon: const Icon(Icons.drag_handle,color: Colors.black,),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-              );
-            })),
-        drawer: Drawer(
-          child: ListView(
-            // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: Text('Drawer Header'),
+          elevation: 0,
+          backgroundColor: Colors.white,
+          leading: Builder(builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(
+                Icons.drag_handle,
+                color: Colors.black,
               ),
-              ListTile(
-                title: const Text('Item 1'),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-              ListTile(
-                title: const Text('Item 2'),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                },
-              ),
-            ],
-          ),
-        ),
-        backgroundColor: Colors.white,
-        body: SlidingUpPanel(
-              panelBuilder: (sc) {
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Icon(Icons.email),
-                    Icon(Icons.email),
-                    Icon(Icons.email),
-                    Icon(Icons.email),
-                  ],
-                );
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
               },
-          body: Column(
-            children: [
-              
-            ],
-          )
-        ));
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          })),
+      drawer: Drawer(
+        child:  DrawerLayout.layout(),
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children:
+        [
+          Text("Welcome", style: TextStyle(fontSize: 28,fontFamily: "Ubuntu"),),
+          Center(
+            child: Container(
+              height: MediaQuery.of(context).size.height/2.2,
+                width: MediaQuery.of(context).size.width,
+                child: RiveAnimation.asset("anim/wave.riv",controllers: [SimpleAnimation('Animation 1')],)),
+          ),
+        ],
+      )
+    );
   }
 }
